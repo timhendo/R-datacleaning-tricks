9b3e82ca14be92e26e096c68338da49365a544cf
timhendo <tim.hendo@gmail.com>
2017-06-12 09:21:00 -0400

    Add files via upload

---
+++ R-datacleaning-tricks.R
@@ -0,0 +1,225 @@
+########################################
+## Tricks for cleaning your data in R 
+##    Christine Zhang  			          
+##     @christinezhang                
+##  ychristinezhang at gmail dot com  
+########################################
+
+## Tricks for cleaning your data in R
+## By Christine Zhang (@christinezhang on Twitter; ychristinezhang at gmail dot com)
+
+# Link to annotated code: 
+# https://github.com/underthecurve/r-data-cleaning-tricks/blob/master/R-datacleaning-tricks.md (markdown file for viewing on the web)
+# https://github.com/underthecurve/r-data-cleaning-tricks/blob/master/R-datacleaning-tricks.pdf (pdf file for printing out)
+
+# Before starting, ensure this .R file and the following data files are in the same folder:
+# "employee-earnings-report-2016.csv"
+# "unemployment.xlsx"
+# "attendees.csv"
+
+# We need to tell R that our files are saved in the same location.
+# In order to do this, you should click through the following:
+# "Session --> Set Working Directory --> To Source File Location"
+
+# In this workshop, I'll show you some examples of real-life "messy" datasets, the problems they present for analysis in R, and the "tidy" solutions to these problems.
+
+#### 1. Finding and replacing non-numeric characters like , and $ ####
+
+salary <- read.csv('employee-earnings-report-2016.csv')
+
+head(salary)
+
+# install.packages('dplyr') # if you don't already have the 'dplyr' package
+library('dplyr') # load the dplyr package
+salary.selected <- select(salary, # the data frame
+                          NAME, DEPARTMENT_NAME, TOTAL.EARNINGS) # the variables to select
+
+names(salary.selected) <- tolower(names(salary.selected)) # change variable names to lowercase
+
+head(salary.selected)
+
+salary.sort <-  arrange(salary.selected, # dataset to sort
+                        total.earnings) # variable to sort by
+salary.sort2 <-  arrange(salary.selected, # dataset to sort
+                        desc(total.earnings)) # variable to sort by
+salary.sort3 <- salary.selected[order(-salary.selected$total.earnings),]
+
+head(salary.sort)
+head(salary.sort2)
+head(salary.sort3)
+
+## What went wrong?
+
+class(salary.selected$total.earnings)
+
+# install.packages('stringr') # if you don't already have the 'stringr' package
+library('stringr') # load the stringr package
+
+salary.selected$total.earningsNUM <- str_replace(
+  salary.selected$total.earnings, # column we want to search
+  pattern = ',', 
+  replacement = '' # what to replace it with
+)
+
+head(salary.selected) # this works - the commas are gone
+
+salary.selected$total.earningsNUM <- str_replace(
+    salary.selected$total.earningsNUM, # column we want to search
+    pattern = '$', # what to find
+    replacement = '' # what to replace it with
+)
+
+head(salary.selected) # this didn't work - the dollar signs are still there
+
+salary.selected$total.earningsNUM <- str_replace(
+    salary.selected$total.earningsNUM, # column we want to search
+    pattern = '\\$', # what to find
+    replacement = '' # what to replace it with
+)
+
+#now drop that extra column
+salary.selected <- select(salary.selected, # the data frame
+                          name, department_name, total.earningsNUM) # the variables to select
+
+
+
+
+# Will this work?
+salary.sort <- arrange(salary.selected, 
+                       desc(total.earningsNUM))
+
+head(salary.selected)
+#why not?
+class(salary.selected$total.earningsNUM)
+
+#it's still a character field let's make it numeric
+
+salary.selected$total.earningsNUM <- as.numeric(salary.selected$total.earningsNUM)
+
+#now check and see if it's a number
+
+class(salary.selected$total.earningsNUM)
+
+#Yes! Now to try out sort again
+
+salary.sort <- arrange(salary.selected,
+                       total.earningsNUM)
+
+head(salary.sort) # ascending order by default
+
+salary.sort <- arrange(salary.selected,
+                       desc(total.earningsNUM)) # descending order
+
+head(salary.sort) # Waiman Lee from the Boston PD is the highest paid city employee
+
+# Now would be a good time to introduce `%>%`, known as the pipe operator.
+# `%>%` is an extremely valuable tool in R, because it allows functions to be chained rather than nested. `%>` looks strange but can be read as "then"—it tells R to do whatever comes after it to the stuff comes before it.
+
+salary.average <- salary.selected %>% # take the salary.selected data frame, THEN
+  group_by(department_name) %>% # group by department_name, THEN
+  summarise(average.earnings = mean(total.earningsNUM)) # calculate the mean of total.earnings for each department_name and name this average.earnings
+
+head(salary.average) # first six rows of average salary by department (alphabetical order)
+
+salary.average %>% filter(department_name == 'Boston Police Department')
+
+# Exercise: The `salary.average` data frame is currently ordered alphabetically by department. How would you sort this dataset by average earnings, from highest to lowest?
+
+
+#optional exercise -- use the n() function to add a county of employees to each grouping
+salary.average <- salary.selected %>% 
+  +     group_by(department_name) %>% 
+  +     summarise(average.earnings = mean(total.earningsNUM), emps=n())
+# now can we sort it high to low again?
+
+#### 2. Merging datasets ####
+
+salary.merged <- merge(x = salary.sort, y = salary.average, by = 'department_name')
+
+head(salary.merged)
+
+#### 3. Reshaping data ####
+
+# install.packages('readxl') # if you don't already have the 'readxl' package
+library('readxl') # load the readxl package
+
+
+unemployment <- read_excel('unemployment.xlsx') 
+
+# install.packages('tidyr') # if you don't already have the 'tidyr' package
+
+library('tidyr') # load the tidyr package
+
+unemployment.long <- gather(unemployment, # data to reshape
+                            Year, # column we want to create from the rows
+                            Rate.Unemployed, # the values of interest
+                            -Country # already a column in the data
+                            )
+
+head(unemployment.long)
+
+class(unemployment.long$Rate.Unemployed) ## "character", not "numeric"
+
+# Why do you think this is? (hint, use `head()` to find out)
+
+unemployment.long$Rate.Unemployed <- as.numeric(unemployment.long$Rate.Unemployed)
+
+str(unemployment.long) # Rate.Unemployed is now "num", which stands for "numeric"
+
+# Exercise: How would we sort `uemployment.long` by Country, then Year using the `arrange() function in `dplyr`?
+
+#### 4. Calculating year-over-year change in panel data ####
+
+unemployment.long <- arrange(unemployment.long, # data frame to sort
+                             Country, Year) # variables to sort by
+
+unemployment.long <- unemployment.long %>% # Take the unemployment.long data frame, THEN
+  arrange(Country, Year) # sort it by Country and then Year.
+
+head(unemployment.long, 5) # First five rows of the data
+
+unemployment.long <- unemployment.long %>% # take the unemployment.long dataset, THEN
+  mutate(Change = Rate.Unemployed - lag(Rate.Unemployed)) # create a variable called Change
+
+head(unemployment.long, 5)
+
+tail(unemployment.long, 5) # last five rows of the data
+
+# Why does Vietnam have a -18.493 percentage point change in 2012?
+
+unemployment.long <- unemployment.long %>% 
+  group_by(Country) %>%
+  mutate(Change = Rate.Unemployed - lag(Rate.Unemployed))
+
+tail(unemployment.long, 5)
+
+#### 5. Recoding numerical variables into categorical ones ####
+
+attendees <- read.csv('attendees.csv', stringsAsFactors = F)
+head(attendees)
+
+table(attendees$Age.group)
+
+attendees$Age.group <- ifelse(attendees$Age.group == '30 - 39', # if attendees$Age.group == '30 - 39'
+                              '30-39', # replace attendees$Age.group with '30-39'
+                              attendees$Age.group) # otherwise, keep attendees$Age.group values the same
+
+table(attendees$Age.group)
+
+table(attendees$Choose.your.status.)
+
+attendees$status <- ifelse(attendees$Choose.your.status. == 'Nonprofit, Academic, Government' |
+                             attendees$Choose.your.status. == 'Nonprofit, Academic, Government Early Bird',
+                           'Nonprofit/Gov', 
+                           attendees$Choose.your.status.)
+table(attendees$status)
+
+#### What else? ####
+
+# - How would you use `ifelse()` and `|` to create a new variable in the `attendees` data (let's call it `status2`) that has just two categories, "Student" and "Other"? 
+                                                                                         
+# - How would you rename the variables in the `attendees` data to make them easier to work with?
+                                                                                         
+# - What are some other issues with this dataset that could be solved using the data cleaning tools we've learned today?  
+                                                                                         
+# - What are some other "messy" data issues you've encountered?
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# R-datacleaning-tricks
+# Tricks for cleaning your data in R
+
+Data + Code for **"Tricks for cleaning your data in R"** at the [Storytelling with Data](https://www.bu.edu/com/data-storytelling/) workshop at Boston University on Tuesday, June 6th 2017.
+
+Equivalent materials for **"Advancing with data visualization in R using ggplot2"** available [here](https://github.com/underthecurve/r-dataviz-ggplot2).
+
+## Links to install R and RStudio
+
+* [R](https://www.r-project.org/): website for the R software
+* [RStudio](https://www.rstudio.com/): website for RStudio, a powerful graphical user interface for R
+
+## Files included
+
+### Annotated code and step-by step instructions for the workshop
+* [R-datacleaning-tricks.md](https://github.com/underthecurve/r-data-cleaning-tricks/blob/master/R-datacleaning-tricks.md): Markdown file (for viewing on the web)
+* [R-datacleaning-tricks.pdf](https://github.com/underthecurve/r-data-cleaning-tricks/blob/master/R-datacleaning-tricks.pdf): PDF file (for printing out)
+
+### R code
+* [R-datacleaning-tricks.R](https://github.com/underthecurve/r-data-cleaning-tricks/blob/master/R-datacleaning-tricks.R): the R code, which can be run in RStudio
+
+### Underlying data needed to run the R code
+* [employee-earnings-report-2016.csv](https://github.com/underthecurve/r-data-cleaning-tricks/blob/master/employee-earnings-report-2016.csv): data on earnings for Boston's municipal employees, from the city's [open data portal](https://data.boston.gov/dataset/employee-earnings-report)
+* [unemployment.xlsx](https://github.com/underthecurve/r-data-cleaning-tricks/blob/master/unemployment.xlsx): data on global unemployment rates from 2012 to 2016, from the [International Monetary Fund](https://www.imf.org/external/pubs/ft/weo/2017/01/weodata/index.aspx)
+* [attendees.csv](https://github.com/underthecurve/r-data-cleaning-tricks/blob/master/attendees.csv): data on some attendees of this workshop, with names and identifying information removed
+
+## How to follow this workshop
+
+* You can clone or download this repository by clicking on the green button above, "Clone or download"
+* Open the `.R` file in RStudio 
+* Follow along by reading the `.md` file online or printing the `.pdf` file out by clicking the Github links above
+
+## Questions / Feedback?
+
+ychristinezhang at gmail dot com
+
+or on Twitter
+
+[@christinezhang](https://twitter.com/christinezhang)
+
+<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
